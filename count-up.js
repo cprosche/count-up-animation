f588acceb26e944d91d74bd911fbff06a45d95e2
Cade Rosche <caderosche@gmail.com>
2021-12-17 16:53:09 -0600

    restructured code

---
+++ count-up.js
@@ -0,0 +1,30 @@
+const animateCountUp = (el) => { // el must be an element containing only an integer
+  
+  const animationDuration = 2; //in seconds
+  const frameRate = 24; //Hz,frames per second AKA number of times number is updated per second
+
+  const endValue = parseInt(el.innerHTML);
+  let counter = 0;
+  const endCounter = animationDuration * frameRate;
+
+  const animationFunction = (end, percent) => {
+    // return end*percent; // linear
+    return end * (percent * (2 - percent)); // ease in
+  };
+
+  let animationInterval = setInterval(() => {
+    const percentComplete = counter / endCounter;
+
+    el.innerHTML = Math.ceil(animationFunction(endValue, percentComplete));
+
+    counter++;
+
+    if (counter > endCounter) {
+      clearInterval(animationInterval);
+    }
+  }, 1000 / frameRate);
+};
+
+const runAnimations = () => {
+  document.querySelectorAll(".countup").forEach(animateCountUp);
+};
